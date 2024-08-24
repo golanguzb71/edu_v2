0fc26e1a2840c82f6bf10ba8032c24a0e8f66e77
golanguzb71 <abdulaziomonov4@gmail.com>
2024-08-24 14:58:20 +0500

    ci-cd-added

--- migrations/mock_information.sql
+++ migrations/mock_information.sql
@@ -6,18 +6,18 @@ VALUES ('123-456-7890', 'John Doe', 123456789.0),
        ('567-890-1234', 'Charlie Davis', 567890123.4);
 
 INSERT INTO COLLECTIONS (TITLE, QUESTIONS_URL)
-VALUES ('Math Quiz', 'https://example.com/math-quiz'),
-       ('Science Quiz', 'https://example.com/science-quiz'),
-       ('History Quiz', 'https://example.com/history-quiz'),
-       ('Geography Quiz', 'https://example.com/geography-quiz'),
-       ('Literature Quiz', 'https://example.com/literature-quiz');
+VALUES ('Math Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
+       ('Science Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
+       ('History Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
+       ('Geography Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
+       ('Literature Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}');
 
 INSERT INTO USER_COLLECTION (USER_ID, ANSWER_FIELD, collection_id)
-VALUES (1, '{"A", "B", "C"}', 2),
-       (2, '{"D", "E", "F"}', 3),
-       (3, '{"G", "H", "I"}', 1),
-       (4, '{"J", "K", "L"}', 2),
-       (5, '{"M", "N", "O"}', 3);
+VALUES (1, '{"A", "B", "C"}', 1),
+       (2, '{"D", "E", "F"}', 1),
+       (3, '{"G", "H", "I"}',1),
+       (4, '{"J", "K", "L"}', 1),
+       (5, '{"M", "N", "O"}', 1);
 
 INSERT INTO GROUPS (name, teacher_name, level, start_time, started_date, days_week)
 VALUES ('Group A', 'Mr. Adams', 'BEGINNER' , '12:00','2024-12-12','ODD_DAYS'),
