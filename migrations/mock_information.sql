727069e9f9f295cc8bf8a122cd24e8467179a843
golanguzb71 <abdulaziomonov4@gmail.com>
2024-08-26 11:47:06 +0500

    collection_fixed

--- migrations/mock_information.sql
+++ migrations/mock_information.sql
@@ -5,19 +5,14 @@ VALUES ('123-456-7890', 'John Doe', 123456789.0),
        ('456-789-0123', 'Bob Brown', 456789012.3),
        ('567-890-1234', 'Charlie Davis', 567890123.4);
 
-INSERT INTO COLLECTIONS (TITLE, QUESTIONS_URL)
-VALUES ('Math Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
-       ('Science Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
-       ('History Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
-       ('Geography Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}'),
-       ('Literature Quiz', '{342cf542-1fba-47dc-8693-0b63678e4c0d.png , 342cf542-1fba-47dc-8693-0b63678e4c0d.png}');
+INSERT INTO COLLECTIONS (TITLE, questions)
+VALUES ('Math Quiz', '1.Which sentences are correct? [qazwsxedc]
+ A)I often play football on Sundays.
+ B)I play often football on Sundays.
+ C)Often I play football on Sundays.');
 
 INSERT INTO USER_COLLECTION (USER_ID, ANSWER_FIELD, collection_id)
-VALUES (1, '{"A", "B", "C"}', 1),
-       (2, '{"D", "E", "F"}', 1),
-       (3, '{"G", "H", "I"}',1),
-       (4, '{"J", "K", "L"}', 1),
-       (5, '{"M", "N", "O"}', 1);
+VALUES (1, '{"A"}', 1);
 
 INSERT INTO GROUPS (name, teacher_name, level, start_time, started_date, days_week)
 VALUES ('Group A', 'Mr. Adams', 'BEGINNER' , '12:00','2024-12-12','ODD_DAYS'),
@@ -27,8 +22,4 @@ VALUES ('Group A', 'Mr. Adams', 'BEGINNER' , '12:00','2024-12-12','ODD_DAYS'),
        ('Group E', 'Prof. Edwards', 'PROFICIENT' , '12:00','2024-12-12','ODD_DAYS');
 
 INSERT INTO ANSWERS (COLLECTION_ID, ANSWER_FIELD)
-VALUES (1, '{"A", "B", "C"}'),
-       (2, '{"D", "E", "F"}'),
-       (3, '{"G", "H", "I"}'),
-       (4, '{"J", "K", "L"}'),
-       (5, '{"M", "N", "O"}');
+VALUES (1, '{"A"}');
