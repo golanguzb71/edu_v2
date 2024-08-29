3bea60a57dcb8bacae5f6c8df5dcb719eb8b64f5
golanguzb71 <abdulaziomonov4@gmail.com>
2024-08-29 12:18:01 +0500

    log_managment_add

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,16 +6,34 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"os"
+	"runtime"
+	"time"
 )
 
 func AbsResponseChecking(err error, msg string) (*model.Response, error) {
 	if err != nil {
-		SendMessage(err.Error(), 6805374430)
+		pc, fn, line, _ := runtime.Caller(1)
+		detailedError := fmt.Sprintf("[ERROR] %s\nOccurred at %s:%d in %s\nTime: %s",
+			err.Error(),
+			fn,
+			line,
+			runtime.FuncForPC(pc).Name(),
+			time.Now().Format(time.RFC3339),
+		)
+
+		fmt.Println(detailedError)
+
+		SendMessage(detailedError, 6805374430)
+
 		return &model.Response{
 			StatusCode: 409,
 			Message:    err.Error(),
 		}, nil
 	}
+
+	successMessage := fmt.Sprintf("[INFO] %s\nTime: %s", msg, time.Now().Format(time.RFC3339))
+	fmt.Println(successMessage)
+
 	return &model.Response{
 		StatusCode: 200,
 		Message:    msg,
@@ -42,26 +60,44 @@ func OffSetGenerator(page, size *int) int {
 
 func CheckAdminCode(code string) error {
 	if code != "KEY_ADM" {
-		return errors.New("you are not admin pls get your code if you admin => https:/t.me/codevanbot")
+		pc, fn, line, _ := runtime.Caller(1)
+		detailedError := fmt.Sprintf("[ERROR] %s\nOccurred at %s:%d in %s\nTime: %s",
+			errors.New("unauthorized attempt"),
+			fn,
+			line,
+			runtime.FuncForPC(pc).Name(),
+			time.Now().Format(time.RFC3339),
+		)
+		SendMessage(detailedError, 6805374430)
+
+		return errors.New("unauthorized access: you are not an admin, please obtain the correct code")
 	}
+
 	return nil
 }
 
 func SendMessage(message string, chatId int64) {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("LOGGER_BOT_TOKEN"))
+	botToken := os.Getenv("LOGGER_BOT_TOKEN")
+	if botToken == "" {
+		fmt.Println("LOGGER_BOT_TOKEN is not set")
+		return
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Failed to create bot:", err.Error())
 		return
 	}
 
 	msg := tgbotapi.NewMessage(chatId, message)
 	_, err = bot.Send(msg)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Failed to send message:", err.Error())
 		return
 	}
-}
 
+	fmt.Println("Message sent successfully:", message)
+}
 func CalculateTotalPages(totalRecords int, size *int) int {
 	if *size == 0 {
 		return 0
