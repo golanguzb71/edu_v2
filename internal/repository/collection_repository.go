edbf7c13d3c05cb31ec8cf9ae5877d332f374786
golanguzb71 <abdulaziomonov4@gmail.com>
2024-08-29 11:58:18 +0500

    OBU_redis

--- internal/repository/collection_repository.go
+++ internal/repository/collection_repository.go
@@ -60,7 +60,7 @@ func (r *CollectionRepository) Delete(id int) error {
 	return nil
 }
 func (r *CollectionRepository) GetCollections() ([]*model.Collection, error) {
-	rows, err := r.db.Query(`SELECT id, title, questions, created_at , is_active FROM collections order by is_active`)
+	rows, err := r.db.Query(`SELECT id, title, questions, created_at , is_active FROM collections order by is_active=false`)
 	if err != nil {
 		return nil, err
 	}
